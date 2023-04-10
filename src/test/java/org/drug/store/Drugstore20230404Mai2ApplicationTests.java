77edf5f5f4d1016719ccd19fa9d1afc3a2b1e8de
maimatsuo <outam.iam812@gmail.com>
2023-04-10 14:42:15 +0900

    first commit

---
+++ src/test/java/org/drug/store/Drugstore20230404Mai2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.drug.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Drugstore20230404Mai2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
